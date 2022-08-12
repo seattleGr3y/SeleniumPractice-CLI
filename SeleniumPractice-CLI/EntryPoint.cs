0b4a1a43ad4e22eb82e5f763c0721661df8f1f47
seattleGr3y <raincityredteam@outlook.com>
2022-08-12 10:45:01 -0700

    updates so now this will do a search on google

--- SeleniumPractice-CLI/EntryPoint.cs
+++ SeleniumPractice-CLI/EntryPoint.cs
@@ -6,40 +6,57 @@ Console.Write("enter the full web address: ");
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
 string siteUrl = Console.ReadLine();
 #pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-Console.Write("enter the name of the elemtent you want to click on: ");
+Console.Write("enter the name of the elemtent where you want to enter text to search: ");
 #pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
 string elemantName = Console.ReadLine();
+Console.Write("enter text you want to search: ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+string txtToSearch = Console.ReadLine();
+// Console.Write("enter the name of the elemtent you want to click on to start the search: ");
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+// string elementName2 = Console.ReadLine();
 
-if (elemantName != null && siteUrl != null)
+if (elemantName != null && siteUrl /* != null && elementName2 */ != null && txtToSearch != null)
 {
-    FirstClass.TrySomeSelenium(elemantName, siteUrl);
+    FirstClass.TrySomeSelenium(elemantName /* elementName2 */, txtToSearch, siteUrl);
 }
 else { Console.WriteLine("what the heck are ya doin?"); }
 
 public static class FirstClass
 {
-    public static bool eNameExists = false;
     public static IWebDriver ChromeDriver = new ChromeDriver($@"{Environment.CurrentDirectory}\..\..\..\Drivers");
+    public static IWebElement? webElement;
+    public static IWebElement? webElement2;
+    public static IWebElement? textBox;
 
-
-    public static void TrySomeSelenium(string elementName, string siteUrl)
+    public static void TrySomeSelenium(string elementName /*, string elementName2 */, string txtToSearch, string siteUrl)
     {
         ChromeDriver.Navigate().GoToUrl(siteUrl);
 
-        if (CheckForElementByName(elementName))
+        if (CheckForElementByName(elementName /*, elementName2 */))
         {
-            ChromeDriver.FindElement(By.ClassName(elementName)).Click();
+            textBox = ChromeDriver.FindElement(By.Name(elementName)); // .SendKeys(txtToSearch))
+            textBox.SendKeys(txtToSearch);
+            textBox.Submit();
+            #region CANNOT USE THIS FOR GOOGLE SEARCH
+            // though this should work I am testing just trying to do a google search
+            // when google.com comes up in the Selenium driver it is slightly different than normal
+            // the button is using different code and is not interactible
+            // I am able though to use .Submit() in the text box and this works just fine
+            // IWebElement searchButton = ChromeDriver.FindElement(By.Name(elementName2));
+            // searchButton.Click();
+            #endregion
         }
-        
         Thread.Sleep(30000);
         ChromeDriver.Quit();
     }
 
-    public static bool CheckForElementByName(string elementName)
+    public static bool CheckForElementByName(string elementName /*, string elementName2 */)
     {
         try
         {
-            IWebElement webElement = ChromeDriver.FindElement(By.Name(elementName));
+            webElement = ChromeDriver.FindElement(By.Name(elementName));
+            // webElement2 = ChromeDriver.FindElement(By.Name(elementName2));
         }
         catch (NoSuchElementException)
         {
